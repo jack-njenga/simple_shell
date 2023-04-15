5813c195145ec0e5b8eb2cc83cbd9d7604d556dd
jack-njenga <jacknjenga99@gmail.com>
2023-04-15 13:27:47 +0300

    task 1 - betty.c

---
+++ betty.c
@@ -0,0 +1,15 @@
+#include "main.h"
+/**
+ * main - A function that passes Betty checks.
+ *
+ * Return: Returns 0 on success otherwise on Error.
+ */
+int main(void)
+{
+	char *str;
+
+	str = "Hello World";
+	printf("%s", str);
+
+	return (0);
+}

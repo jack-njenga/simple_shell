9912baafe05e8d211e3ab923afc33c8de84753bb
jack-njenga <jacknjenga99@gmail.com>
2023-04-21 16:18:07 +0300

    _getline

---
+++ _getline.c
@@ -0,0 +1,47 @@
+#include "main.h"
+/**
+ */
+ssize_t _getline(char **buffer, size_t *size, FILE *stream)
+{
+	char buff[1024];
+	ssize_t ret;
+	int fd;
+	static int sz = 0;
+
+	sz = sizeof(buff);
+	fd = fileno(stream);
+	ret = read(fd, buff, sz);
+
+	printf("buff len = %ld\n", strlen(buff));
+
+	*buffer = malloc(strlen(buff) + 1);
+	if (*buffer == NULL)
+	{
+		perror("malloc");
+		return (-1);
+	}
+	strcpy(*buffer, buff);
+
+	if (ret == -1)
+		perror("read");
+	return (ret);
+}
+
+int main(void)
+{
+	char *buff = NULL;
+	size_t size = 0;
+
+	while (1)
+	{
+		printf("ENTER ");
+
+		_getline(&buff, &size, stdin);
+
+		printf("===>> %s\n", buff);
+	}
+	free(buff);
+	return (0);
+}
+
+

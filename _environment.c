61b13e9ba3a1cffa7232632161dcbb42a6929305
Annet <annetnasimiyuchebukati@gmail.com>
2023-04-28 08:57:37 -0700

    betty

--- _environment.c
+++ _environment.c
@@ -1,37 +1,41 @@
 #include "main.h"
 
-int my_setenv(const char *name, const char *value)
+int my_setenv(const char *nm, const char *val)
 {
-    if (name == NULL || value == NULL || strchr(name, '=') != NULL)
-    {
-        fprintf(stderr, "Invalid arguments\n");
-        return (-1);
-    }
+	int ret;
 
-    int ret = setenv(name, value, 1);
-    if (ret != 0)
-    {
-        perror("setenv");
-        return (-1);
-    }
+	if (nm == NULL || val == NULL || strchr(nm, '=') != NULL)
+	{
+		fprintf(stderr, "Invalid arguments\n");
+		return (-1);
+	}
 
-    return (0);
+	ret = setenv(nm, val, 1);
+	if (ret != 0)
+	{
+		perror("setenv");
+		return (-1);
+	}
+
+	return (0);
 }
 
-int my_unsetenv(const char *name)
+int my_unsetenv(const char *nm)
 {
-    if (name == NULL || strchr(name, '=') != NULL)
-    {
-        fprintf(stderr, "Invalid arguments\n");
-        return (-1);
-    }
+	int ret;
 
-    int ret = unsetenv(name);
-    if (ret != 0)
-    {
-        perror("unsetenv");
-        return (-1);
-    }
+	if (nm == NULL || strchr(nm, '=') != NULL)
+	{
+		fprintf(stderr, "Invalid arguments\n");
+		return (-1);
+	}
 
-    return (0);
+	ret = unsetenv(nm);
+	if (ret != 0)
+	{
+		perror("unsetenv");
+		return (-1);
+	}
+
+	return (0);
 }
